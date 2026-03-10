90d9dddaf56093e6923bbca78111ced0be03832f
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-03-11 00:58:24 +0300

    commit

--- QuickStart.WebUI/ViewComponents/_DefaultContactComponentPartial.cs
+++ QuickStart.WebUI/ViewComponents/_DefaultContactComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuickStart.WebUI.Dtos.Contacts;
 
 namespace QuickStart.WebUI.ViewComponents
 {
     public class _DefaultContactComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultContactComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7051/api/Contact");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactsDto>>(jsonData);
+                var firstItem = values?.FirstOrDefault();
+                return View(firstItem);
+            }
+            return View(new ResultContactsDto());
         }
     }
 }
